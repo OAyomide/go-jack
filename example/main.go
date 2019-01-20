0796a224bb63e85e06bd9dde9c051f94dde6567c
Ayomide <onigbindeayomide@gmail.com>
2019-01-20 11:44:40 +0100

    initialize creating sample bot from commandline

---
+++ example/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "github.com/oayomide/messenger"
+
+var verifyT, accessT, appS
+
+var (
+	verifyToken = flag.String("verify-token", verifyT, "The token used to verify facebook (required)")
+	verify      = flag.Bool("should-verify", false, "Whether or not the app should verify itself")
+	pageToken   = flag.String("page-token", accessT, "The token that is used to verify the page on facebook")
+	appSecret   = flag.String("app-secret", appS, "The app secret from the facebook developer portal (required)")
+	host        = flag.String("host", "localhost", "The host used to serve the messenger bot")
+	port        = flag.Int("port", 4000, "The port used to serve the messenger bot")
+)
+
+
+var bot = messenger.New(messenger.Options{
+	Verify:      *verify,
+	AppSecret:   *appSecret,
+	VerifyToken: *verifyToken,
+	Token:       *pageToken,
+	WebhookURL:  "/api/webhook",
+})
+
+
+func main() {
+	flag.Parse()
+
+	if *verifyToken == "" || *appSecret == "" || *pageToken == "" {
+		fmt.Println("There seem to be fields that are empty. Please make sure none is empty")
+		fmt.Println()
+		flag.Usage()
+
+		os.Exit(-1)
+	}
+
+	// Setup a handler to be triggered when a message is received
+	bot.HandleMessage(func(m messenger.Message, r *messenger.Response) {
+		fmt.Printf("%v (Sent, %v)\n", m.Text, m.Time.Format(time.UnixDate))
+
+		p, err := bot.ProfileByID(m.Sender.ID, []string{"name", "first_name", "last_name", "profile_pic"})
+		if err != nil {
+			fmt.Println("Something went wrong!", err)
+		}
+
+		r.Text(fmt.Sprintf("Hello, %v!", p.FirstName), messenger.ResponseType)
+	})
+
+	// Setup a handler to be triggered when a message is delivered
+	bot.HandleDelivery(func(d messenger.Delivery, r *messenger.Response) {
+		fmt.Println("Delivered at:", d.Watermark().Format(time.UnixDate))
+	})
+
+	// Setup a handler to be triggered when a message is read
+	bot.HandleRead(func(m messenger.Read, r *messenger.Response) {
+		fmt.Println("Read at:", m.Watermark().Format(time.UnixDate))
+	})
+
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	log.Println("Serving messenger bot on", addr)
+	log.Fatal(http.ListenAndServe(addr, bot.Handler()))
+}
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+
+	logger "github.com/go-jack/logger"
 )
 
 //Bot struct is the struct our bot property will be made after
@@ -19,11 +22,75 @@ type Bot struct {
 
 // declare our CLI flags
 var (
-	help = flag.String("help", "", "Display the help info about the framework")
+	mainCommand = flag.NewFlagSet("new", flag.ExitOnError)
+
+	// // create subcommand for command new
+	name = mainCommand.String("name", "", "Creates a new project with the specified name")
+	full = mainCommand.Bool("full", true, "Create a project with all config out of the box")
 )
 
 func main() {
-	flag.Parse()
 
-	fmt.Printf("User inputs: %s", *help)
+	// make sure the subcommand has been passed
+	if len(os.Args) < 2 {
+		fmt.Println("Please specify a project structure to create")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "new":
+		mainCommand.Parse(os.Args[2:])
+
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// check which command was parsed
+	if mainCommand.Parsed() {
+		if *name == "" {
+			fmt.Println("Specify a name for the new project")
+			mainCommand.PrintDefaults()
+			os.Exit(1)
+		}
+	}
+}
+
+func createScaffold(foldername string) {
+	// we want to create the folder first
+
+	err := os.Mkdir(foldername, 777)
+
+	if err != nil {
+		logger.Print("error", "Couldn't create folder for the new project", err.Error())
+	}
+
+	// we want to copy the files from our example
+	file, err := os.Create(foldername + "/" + "main.go")
+	if err != nil {
+		logger.Print("error", "Error creating the main.go file")
+	}
+
+	// we want to read the main.go file we're copying from
+	fl, err := os.Open("actions/main.go")
+	if err != nil {
+		logger.Print("error", "ERROR OPENING THE EXAMPLE main.go FILE", err.Error())
+		return
+	}
+	defer fl.Close()
+
+	flInfo, err := fl.Stat()
+	if err != nil {
+		logger.Print("error", "ERROR GETTING STATS FOR THE OPENED FILE", err.Error())
+		return
+	}
+
+	flSize := flInfo.Size()
+	fileBuffer := make([]byte, flSize)
+
+	bytesRead, err := fl.Read(fileBuffer)
+	if err != nil {
+		logger.Print("error", "ERROR READING FILE", err.Error())
+		return
+	}
 }
